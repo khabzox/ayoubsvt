eba04a7f43fa49d414158f4265b2effba68d32b0
Abdelkabir Ouadoukou <152230716+khabzo@users.noreply.github.com>
2024-03-08 16:55:26 +0100

    Rebuilding All files

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
-import Hero from '@/components/Hero/page';
-import Levels from '@/components/Levels/page';
-import Header from '@/components/Header/page';
-import Feature from "@/components/Features/page";
-import { LaboShow } from "@/components/LaboShow/page";
+// import Hero from '@/components/Hero/page';
+// import Levels from '@/components/Levels/page';
+// import Header from '@/components/Header/page';
+// import Feature from "@/components/Features/page";
+// import { LaboShow } from "@/components/LaboShow/page";
 import Image from 'next/image';
 // import { GoogleGeminiEffectDemo } from "@/components/GoogleGeminiEffectDemo/page";
 // <GoogleGeminiEffectDemo />
