f59bf7a52155ed04a743c70344617d3589988b72
Abdelkabir Ouadoukou <152230716+khabzo@users.noreply.github.com>
2024-03-08 16:46:08 +0100

    Rebuilding All files

--- app/page.tsx
+++ app/page.tsx
@@ -3,18 +3,23 @@ import Levels from '@/components/Levels/page';
 import Header from '@/components/Header/page';
 import Feature from "@/components/Features/page";
 import { LaboShow } from "@/components/LaboShow/page";
+import Image from 'next/image';
 // import { GoogleGeminiEffectDemo } from "@/components/GoogleGeminiEffectDemo/page";
 // <GoogleGeminiEffectDemo />
 export default function Home() {
   return (
     <>
-    <Header /> 
+    <div className='text-center mt-64'>
+      <img src='./favicon.ico' height={90} width={90} className='inline-flex'></img>
+      <h1 className='mt-5'>En cours de reconstruction...</h1>
+    </div>
+    {/* <Header /> 
     <main>
       <Hero />
       <Levels />
       <Feature />
       <LaboShow />
-    </main>
+    </main> */}
     </>
   );
 } 
