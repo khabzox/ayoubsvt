4b50d5ff9da10d19e41a0736af16a7b373f96a6f
Abdelkabir Ouadoukou <152230716+khabzo@users.noreply.github.com>
2024-03-11 17:54:45 +0000

    Disclaimer ||  Term of Condition || Privacy Policy ||  Contact US || About Us pages build!

---
+++ app/(Disclaimer)/Disclaimer/page.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from 'next';
+export const metadata: Metadata = {
+    title: "AyoubSVT | Disclaimer",
+    description: "Découvrez les informations importantes concernant la clause de non-responsabilité sur AyoubSVT. Notre page de déni de responsabilité offre des détails transparents sur l'utilisation du site, les garanties et les limitations. Consultez notre disclaimer pour comprendre pleinement nos conditions d'utilisation et nos engagements envers nos utilisateurs.",
+    keywords: "ayoubsvt.com , Disclaimer ayoubsvt , Disclaimer",
+};
+
+export default function Disclaimer() {
+    return (
+        <>
+        <div className="mx-6 my-10">
+        <h1 className="text-black text-4xl mb-4 text-3xl font-semibold">Disclaimer</h1>
+
+<p className="text-black font-sans text-lg text-2xl pt-4 pb-2 text-left">If you require any more information or have any questions about our site's disclaimer, please feel free to contact us by email at benhmiedayoub2@gmail.com. Our Disclaimer was generated with the help of the <a href="https://www.disclaimergenerator.net/">Free Disclaimer Generator</a>.</p>
+
+<h2 className="text-black text-2xl pt-8 text-left font-medium">Disclaimers for <span className="text-primaryy underline"><a href="https://www.AyoubSVT.com/">www.AyoubSVT.com</a></span></h2>
+
+<p className="text-black font-sans text-lg text-2xl pt-4 pb-2 text-left">All the information on this website - <span className="text-primaryy underline"><a href="https://www.AyoubSVT.com/">www.AyoubSVT.com</a></span> - is published in good faith and for general information purpose only. <span className="text-primaryy underline"><a href="https://www.AyoubSVT.com/">www.AyoubSVT.com</a></span> does not make any warranties about the completeness, reliability and accuracy of this information. Any action you take upon the information you find on this website (<span className="text-primaryy underline"><a href="https://www.AyoubSVT.com/">www.AyoubSVT.com</a></span>), is strictly at your own risk. <span className="text-primaryy underline"><a href="https://www.AyoubSVT.com/">www.AyoubSVT.com</a></span> will not be liable for any losses and/or damages in connection with the use of our website.</p>
+
+<p className="text-black font-sans text-lg text-2xl pt-4 pb-2 text-left">From our website, you can visit other websites by following hyperlinks to such external sites. While we strive to provide only quality links to useful and ethical websites, we have no control over the content and nature of these sites. These links to other websites do not imply a recommendation for all the content found on these sites. Site owners and content may change without notice and may occur before we have the opportunity to remove a link which may have gone 'bad'.</p>
+
+<p className="text-black font-sans text-lg text-2xl pt-4 pb-2 text-left">Please be also aware that when you leave our website, other sites may have different privacy policies and terms which are beyond our control. Please be sure to check the Privacy Policies of these sites as well as their "Terms of Service" before engaging in any business or uploading any information.</p>
+
+<h2 className="text-black text-2xl pt-8 text-left font-medium">Consent</h2>
+
+<p className="text-black font-sans text-lg text-2xl pt-4 pb-2 text-left">By using our website, you hereby consent to our disclaimer and agree to its terms.</p>
+
+<h2 className="text-black text-2xl pt-8 text-left font-medium">Update</h2>
+
+<p className="text-black font-sans text-lg text-2xl pt-4 pb-2 text-left">Should we update, amend or make any changes to this document, those changes will be prominently posted here.</p>
+        </div>
+        </>
+    );
+};
