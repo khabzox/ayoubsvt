8a2d70b9ff3b7191335d106036e0194e02efd2b5
Abdelkabir Ouadoukou <152230716+khabzo@users.noreply.github.com>
2024-03-11 18:12:03 +0000

    Disclaimer ||  Term of Condition || Privacy Policy ||  Contact US || About Us pages build!

--- middleware.ts
+++ middleware.ts
@@ -6,7 +6,7 @@ import { authMiddleware } from "@clerk/nextjs";
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
-    publicRoutes: ['/']
+    publicRoutes: ['/', '/About-Us', '/Contact-US', '/Privacy-Policy', '/Term-of-Condition', '/Disclaimer']
 });
  
 export const config = {
