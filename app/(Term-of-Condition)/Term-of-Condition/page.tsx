2e404643467b68449880ea1711acd74d0e53b675
Abdelkabir Ouadoukou <152230716+khabzo@users.noreply.github.com>
2024-03-11 17:59:23 +0000

    Disclaimer ||  Term of Condition || Privacy Policy ||  Contact US || About Us pages build!

--- app/(Term-of-Condition)/Term-of-Condition/page.tsx
+++ app/(Term-of-Condition)/Term-of-Condition/page.tsx
@@ -13,17 +13,17 @@ export default function TermofCondition() {
 
 <p className="text-black font-sans text-lg text-2xl pt-4 pb-2 text-left">Welcome to <span className="text-primaryy underline"><a href="https://www.AyoubSVT.com/">www.AyoubSVT.com</a></span>!</p>
 
-<p className="text-black font-sans text-lg text-2xl pt-4 pb-2 text-left">These terms and conditions outline the rules and regulations for the use of AyoubSVT's Website, located at <span className="text-primaryy underline"><a href="https://www.AyoubSVT.com/">www.AyoubSVT.com</a></span>.</p>
+<p className="text-black font-sans text-lg text-2xl pt-4 pb-2 text-left">These terms and conditions outline the rules and regulations for the use of AyoubSVT&lsquo;s Website, located at <span className="text-primaryy underline"><a href="https://www.AyoubSVT.com/">www.AyoubSVT.com</a></span>.</p>
 
 <p className="text-black font-sans text-lg text-2xl pt-4 pb-2 text-left">By accessing this website we assume you accept these terms and conditions. Do not continue to use <span className="text-primaryy underline"><a href="https://www.AyoubSVT.com/">www.AyoubSVT.com</a></span> if you do not agree to take all of the terms and conditions stated on this page.</p>
 
-<p className="text-black font-sans text-lg text-2xl pt-4 pb-2 text-left">The following terminology applies to these Terms and Conditions, Privacy Statement and Disclaimer Notice and all Agreements: "Client", "You" and "Your" refers to you, the person log on this website and compliant to the Company's terms and conditions. "The Company", "Ourselves", "We", "Our" and "Us", refers to our Company. "Party", "Parties", or "Us", refers to both the Client and ourselves. All terms refer to the offer, acceptance and consideration of payment necessary to undertake the process of our assistance to the Client in the most appropriate manner for the express purpose of meeting the Client's needs in respect of provision of the Company's stated services, in accordance with and subject to, prevailing law of ma. Any use of the above terminology or other words in the singular, plural, capitalization and/or he/she or they, are taken as interchangeable and therefore as referring to same.</p>
+<p className="text-black font-sans text-lg text-2xl pt-4 pb-2 text-left">The following terminology applies to these Terms and Conditions, Privacy Statement and Disclaimer Notice and all Agreements: "Client", "You" and "Your" refers to you, the person log on this website and compliant to the Company&lsquo;s terms and conditions. "The Company", "Ourselves", "We", "Our" and "Us", refers to our Company. "Party", "Parties", or "Us", refers to both the Client and ourselves. All terms refer to the offer, acceptance and consideration of payment necessary to undertake the process of our assistance to the Client in the most appropriate manner for the express purpose of meeting the Client&lsquo;s needs in respect of provision of the Company&lsquo;s stated services, in accordance with and subject to, prevailing law of ma. Any use of the above terminology or other words in the singular, plural, capitalization and/or he/she or they, are taken as interchangeable and therefore as referring to same.</p>
 
 <h3 className="text-black text-2xl pt-8 text-left font-medium"><strong>Cookies</strong></h3>
 
-<p className="text-black font-sans text-lg text-2xl pt-4 pb-2 text-left">We employ the use of cookies. By accessing <span className="text-primaryy underline"><a href="https://www.AyoubSVT.com/">www.AyoubSVT.com</a></span>, you agreed to use cookies in agreement with the AyoubSVT's Privacy Policy. </p>
+<p className="text-black font-sans text-lg text-2xl pt-4 pb-2 text-left">We employ the use of cookies. By accessing <span className="text-primaryy underline"><a href="https://www.AyoubSVT.com/">www.AyoubSVT.com</a></span>, you agreed to use cookies in agreement with the AyoubSVT&lsquo;s Privacy Policy. </p>
 
-<p className="text-black font-sans text-lg text-2xl pt-4 pb-2 text-left">Most interactive websites use cookies to let us retrieve the user's details for each visit. Cookies are used by our website to enable the functionality of certain areas to make it easier for people visiting our website. Some of our affiliate/advertising partners may also use cookies.</p>
+<p className="text-black font-sans text-lg text-2xl pt-4 pb-2 text-left">Most interactive websites use cookies to let us retrieve the user&lsquo;s details for each visit. Cookies are used by our website to enable the functionality of certain areas to make it easier for people visiting our website. Some of our affiliate/advertising partners may also use cookies.</p>
 
 <h3 className="text-black text-2xl pt-8 text-left font-medium"><strong>License</strong></h3>
 
@@ -66,7 +66,7 @@ export default function TermofCondition() {
     <li className="text-black font-sans text-lg pt-2 text-left">System wide Accredited Businesses except soliciting non-profit organizations, charity shopping malls, and charity fundraising groups which may not hyperlink to our Web site.</li>
 </ul>
 
-<p className="text-black font-sans text-lg  text-2xl pt-8 text-left">These organizations may link to our home page, to publications or to other Website information so long as the link: (a) is not in any way deceptive; (b) does not falsely imply sponsorship, endorsement or approval of the linking party and its products and/or services; and (c) fits within the context of the linking party's site.</p>
+<p className="text-black font-sans text-lg  text-2xl pt-8 text-left">These organizations may link to our home page, to publications or to other Website information so long as the link: (a) is not in any way deceptive; (b) does not falsely imply sponsorship, endorsement or approval of the linking party and its products and/or services; and (c) fits within the context of the linking party&lsquo;s site.</p>
 
 <p className="text-black font-sans text-lg  text-2xl pt-8 text-left">We may consider and approve other link requests from the following types of organizations:</p>
 
@@ -82,7 +82,7 @@ export default function TermofCondition() {
 
 <p className="text-black font-sans text-lg  text-2xl pt-8 text-left">We will approve link requests from these organizations if we decide that: (a) the link would not make us look unfavorably to ourselves or to our accredited businesses; (b) the organization does not have any negative records with us; (c) the benefit to us from the visibility of the hyperlink compensates the absence of AyoubSVT; and (d) the link is in the context of general resource information.</p>
 
-<p className="text-black font-sans text-lg  text-2xl pt-8 text-left">These organizations may link to our home page so long as the link: (a) is not in any way deceptive; (b) does not falsely imply sponsorship, endorsement or approval of the linking party and its products or services; and (c) fits within the context of the linking party's site.</p>
+<p className="text-black font-sans text-lg  text-2xl pt-8 text-left">These organizations may link to our home page so long as the link: (a) is not in any way deceptive; (b) does not falsely imply sponsorship, endorsement or approval of the linking party and its products or services; and (c) fits within the context of the linking party&lsquo;s site.</p>
 
 <p className="text-black font-sans text-lg  text-2xl pt-8 text-left">If you are one of the organizations listed in paragraph 2 above and are interested in linking to our website, you must inform us by sending an e-mail to AyoubSVT. Please include your name, your organization name, contact information as well as the URL of your site, a list of any URLs from which you intend to link to our Website, and a list of the URLs on our site to which you would like to link. Wait 2-3 weeks for a response.</p>
 
@@ -91,10 +91,10 @@ export default function TermofCondition() {
 <ul className="list-decimal pl-8 mb-4">
     <li className="text-black font-sans text-lg pt-2 text-left">By use of our corporate name; or</li>
     <li className="text-black font-sans text-lg pt-2 text-left">By use of the uniform resource locator being linked to; or</li>
-    <li className="text-black font-sans text-lg pt-2 text-left">By use of any other description of our Website being linked to that makes sense within the context and format of content on the linking party's site.</li>
+    <li className="text-black font-sans text-lg pt-2 text-left">By use of any other description of our Website being linked to that makes sense within the context and format of content on the linking party&lsquo;s site.</li>
 </ul>
 
-<p className="text-black font-sans text-lg  text-2xl pt-8 text-left">No use of AyoubSVT's logo or other artwork will be allowed for linking absent a trademark license agreement.</p>
+<p className="text-black font-sans text-lg  text-2xl pt-8 text-left">No use of AyoubSVT&lsquo;s logo or other artwork will be allowed for linking absent a trademark license agreement.</p>
 
 <h3 className="text-black font-medium text-2xl pt-8 text-left"><strong>iFrames</strong></h3>
 
@@ -106,7 +106,7 @@ export default function TermofCondition() {
 
 <h3 className="text-black font-medium text-2xl pt-8 text-left"><strong>Reservation of Rights</strong></h3>
 
-<p className="text-black font-sans text-lg  text-2xl pt-8 text-left">We reserve the right to request that you remove all links or any particular link to our Website. You approve to immediately remove all links to our Website upon request. We also reserve the right to amen these terms and conditions and it's linking policy at any time. By continuously linking to our Website, you agree to be bound to and follow these linking terms and conditions.</p>
+<p className="text-black font-sans text-lg  text-2xl pt-8 text-left">We reserve the right to request that you remove all links or any particular link to our Website. You approve to immediately remove all links to our Website upon request. We also reserve the right to amen these terms and conditions and it&lsquo;s linking policy at any time. By continuously linking to our Website, you agree to be bound to and follow these linking terms and conditions.</p>
 
 <h3 className="text-black font-medium text-2xl pt-8 text-left"><strong>Removal of links from our website</strong></h3>
 
