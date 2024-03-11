de446b193b6e75e92ab816e92f0ff9298496a071
Abdelkabir Ouadoukou <152230716+khabzo@users.noreply.github.com>
2024-03-11 18:07:50 +0000

    Disclaimer ||  Term of Condition || Privacy Policy ||  Contact US || About Us pages build!

--- app/(Disclaimer)/Disclaimer/page.tsx
+++ app/(Disclaimer)/Disclaimer/page.tsx
@@ -19,7 +19,7 @@ export default function Disclaimer() {
 
 <p className="text-black font-sans text-lg text-2xl pt-4 pb-2 text-left">From our website, you can visit other websites by following hyperlinks to such external sites. While we strive to provide only quality links to useful and ethical websites, we have no control over the content and nature of these sites. These links to other websites do not imply a recommendation for all the content found on these sites. Site owners and content may change without notice and may occur before we have the opportunity to remove a link which may have gone &lsquo;bad&lsquo;.</p>
 
-<p className="text-black font-sans text-lg text-2xl pt-4 pb-2 text-left">Please be also aware that when you leave our website, other sites may have different privacy policies and terms which are beyond our control. Please be sure to check the Privacy Policies of these sites as well as their "Terms of Service" before engaging in any business or uploading any information.</p>
+<p className="text-black font-sans text-lg text-2xl pt-4 pb-2 text-left">Please be also aware that when you leave our website, other sites may have different privacy policies and terms which are beyond our control. Please be sure to check the Privacy Policies of these sites as well as their &ldquo;Terms of Service&ldquo; before engaging in any business or uploading any information.</p>
 
 <h2 className="text-black text-2xl pt-8 text-left font-medium">Consent</h2>
 
