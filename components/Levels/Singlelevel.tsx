c4a28aa23a899f7dcc7db8add32951eadc5d4c60
Abdelkabir Ouadoukou <152230716+khabzo@users.noreply.github.com>
2024-03-15 00:28:17 +0000

    build Desktop & mobile application

--- components/Levels/Singlelevel.tsx
+++ components/Levels/Singlelevel.tsx
@@ -28,14 +28,8 @@ const SingleBrand = ({ brand }: { brand: Brand }) => {
         className="animate_top mx-w-full relative block h-25 w-[98px]"
       >
         <Image
-          className="opacity-65 transition-all duration-300 hover:opacity-100 dark:hidden"
+          className="opacity-65 transition-all duration-300 hover:opacity-100"
           src={image}
-          alt={name}
-          fill
-        />
-        <Image
-          className="hidden opacity-50 transition-all duration-300 hover:opacity-100 dark:block"
-          src={imageLight}
           alt={name}
           fill
         />
--- components/Levels/page.tsx
+++ components/Levels/page.tsx
@@ -9,7 +9,7 @@ const Brands = () => {
       {/* <!-- ===== Clients Start ===== --> 
     border border-x-0 
     */}
-      <section className=" py-11 ">
+      <section className="py-11">
         <div className="mx-auto max-w-c-1390 px-4 md:px-8 2xl:px-0">
           <div className="grid grid-cols-3 items-center justify-center gap-7.5 md:grid-cols-6 lg:gap-12.5 xl:gap-29">
             {brandData.map((brand, key) => (
