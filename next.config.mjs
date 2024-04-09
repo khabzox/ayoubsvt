183e9c9cf41cde223804f49962d7d3b628904f20
Abdelkabir Ouadoukou <152230716+khabzo@users.noreply.github.com>
2024-04-09 22:20:25 +0000

    Update all files from begin (maintenance)

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,4 @@
-const withPWA = require('next-pwa')({
-  dest: 'public'
-})
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-};
+const nextConfig = {};
 
-module.exports = withPWA(nextConfig);
+export default nextConfig;
