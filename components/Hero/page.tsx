955ec087ce50b09be595770893db99fc1e138057
Abdelkabir Ouadoukou <abdelkabir.ouadoukou@gmail.com>
2024-04-22 02:18:07 +0100

    style: fix styling of button

--- components/Hero/page.tsx
+++ components/Hero/page.tsx
@@ -11,7 +11,7 @@ const Hero = () => {
   return (
 <section>
     <div className="pt-8 px-4 mx-auto max-w-screen-xl text-center lg:pt-16 lg:px-12">
-        <Link href="/dashboard" className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 inline-flex justify-between items-center py-1 px-1 pr-4 mb-7 text-sm text-gray-700 bg-gray-100 rounded-full dark:bg-gray-800 dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700" role="alert">
+        <Link href="/dashboard" className="text-xs bg-light rounded-full text-black px-4 py-1.5 mr-3">
             <span className="text-xs bg-light rounded-full text-black px-4 py-1.5 mr-3">New</span>
             <span className="text-sm font-medium text-white">Virtual-ZX10 un Nouveau Labo</span> 
             <svg className="ml-2 w-5 h-5 text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd"></path></svg>
