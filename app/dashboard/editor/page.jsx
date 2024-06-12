398f9baa008d2b89b732de04814f79eea8d5dc38
Abdelkabir Ouadoukou <152230716+khabzox@users.noreply.github.com>
2024-06-12 17:35:51 +0100

    build: editor v1

--- app/dashboard/editor/page.jsx
+++ app/dashboard/editor/page.jsx
@@ -1,8 +1,35 @@
-import UploeadFiles from "@/components/dashboard/Editor/UploeadFiles";
-export default function Editor() {
+"use client";
+// import UploeadFiles from "@/components/dashboard/Editor/UploeadFiles";
+import { useEffect, useRef } from "react";
+// import WebViewer from "@pdftron/webviewer"
+
+export default function HomePage() {
+  const viewer = useRef(null);
+
+  useEffect(() => {
+    import("@pdftron/webviewer").then(() => {
+      WebViewer(
+        {
+          path: "/lib",
+          enableOfficeEditing: true,
+          //   initialDoc: "https://github.com/PDFTron/webviewer-nextjs-sample/blob/a8a422ff3a57483bd12360278bacf6d9e5f4ba6e/public/files/PDFTRON_about.pdf",
+        },
+        viewer.current
+      ).then((instance) => {
+        const { docViewer } = instance;
+        // you can now call WebViewer APIs here...
+      });
+    });
+  }, []);
+
   return (
-    <>
-	  <UploeadFiles />
-    </>
+    <div className="MyComponent">
+      {/* <UploeadFiles /> */}
+      <div
+        className="webviewer"
+        ref={viewer}
+        style={{ height: "90.774299835255354vh" }}
+      ></div>
+    </div>
   );
 }
