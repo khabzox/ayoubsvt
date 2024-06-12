bccd6fc731bb0f16071975b8b9a5e8a5c569ff61
Abdelkabir Ouadoukou <152230716+khabzox@users.noreply.github.com>
2024-06-12 13:49:17 +0100

    build: editor  (not finish yet)

--- app/dashboard/editor/page.jsx
+++ app/dashboard/editor/page.jsx
@@ -1,26 +1,26 @@
-'use client';
-import { useEffect, useRef } from 'react';
+"use client";
+import { useEffect, useRef } from "react";
 
 export default function Home() {
-	const containerRef = useRef(null);
+  const containerRef = useRef(null);
 
-	useEffect(() => {
-		const container = containerRef.current;
-	
-		if (typeof window !== 'undefined') {
-		  import('pspdfkit').then((PSPDFKit) => {
-			if (PSPDFKit) {
-			  PSPDFKit.unload(container);
-			}
-	
-			PSPDFKit.load({
-			  container,
-			  document: '/document.pdf',
-			  baseUrl: `${window.location.protocol}//${window.location.host}/`,
-			});
-		  });
-		}
-	  }, []);
+  useEffect(() => {
+    const container = containerRef.current;
 
-	return <div ref={containerRef} style={{ height: '100vh' }} />;
+    if (typeof window !== "undefined") {
+      import("pspdfkit").then((PSPDFKit) => {
+        if (PSPDFKit) {
+          PSPDFKit.unload(container);
+        }
+
+        PSPDFKit.load({
+          container,
+          document: "/document.pdf",
+          baseUrl: `${window.location.protocol}//${window.location.host}/`,
+        });
+      });
+    }
+  }, []);
+
+  return <div ref={containerRef} style={{ height: "100vh" }} />;
 }
