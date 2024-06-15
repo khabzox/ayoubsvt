edccaf01264b51a9bcb5da6b4c7b56a851dd403e
Abdelkabir Ouadoukou <152230716+khabzox@users.noreply.github.com>
2024-06-15 16:04:03 +0100

    fix: stuep OpenGraph

---
+++ app/dashboard/tutorials/layout.jsx
@@ -0,0 +1,9 @@
+export const metadata = {
+  title: "AyoubSVT | Tutoriels",
+  description:
+    "Tutoriels propose une large gamme de tutoriels couvrant divers sujets en sciences et sciences de la vie. Ces guides pas à pas sont conçus pour aider les apprenants de tous niveaux à comprendre des concepts complexes et à améliorer leurs compétences pratiques.",
+};
+
+export default function RootLayout({ children }) {
+  return <div>{children}</div>;
+}
--- app/dashboard/tutorials/page.jsx
+++ app/dashboard/tutorials/page.jsx
@@ -7,10 +7,7 @@ import {
   BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb";
 import TutorielsDsignView from "@/components/dashboard/Tutoriels/TutorielsDsignView";
-export const metadata = {
-  title: "AyoubSVT | Tutoriels",
-  description: "Tutoriels propose une large gamme de tutoriels couvrant divers sujets en sciences et sciences de la vie. Ces guides pas à pas sont conçus pour aider les apprenants de tous niveaux à comprendre des concepts complexes et à améliorer leurs compétences pratiques." ,
-};
+
 export default function Tutorials() {
   return (
     <>
