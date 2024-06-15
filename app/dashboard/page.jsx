86e6bfe4fb522154220a8304720f33d6ff988db2
Abdelkabir Ouadoukou <152230716+khabzox@users.noreply.github.com>
2024-06-15 16:44:41 +0100

    style: add some style to dashboard page

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -4,7 +4,11 @@ import DashboardViwer from "@/components/dashboard/DashboardViwer";
 export default function Home() {
   return (
     <>
-    <DashboardViwer />
+      <h1 className="flex justify-center mt-[40vh] text-center font-bold text-primLaboHov">
+        Bienvenue 🥳🚀<br />
+        Sur le Tableau de Bord d&apos;AyoubSVT
+      </h1>
+      {/* <DashboardViwer /> */}
     </>
   );
 }
